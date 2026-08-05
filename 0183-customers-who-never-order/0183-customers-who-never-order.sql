52d2c95c4b857a24e22712bfaa7325167dd5d6c7
Ketan Sonbawane <sonbawaneketan@gmail.com>
2026-08-05 14:02:11 +0530

    Time: 662 ms (25.13%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,5 +1,6 @@
-SELECT name as Customers 
-from customers as c
-left join orders as o 
-on c.id = o.customerId
+select name as Customers
+
+from Customers as c
+left join Orders as o
+on c.id = o.CustomerId
 where customerId is null
