2615de2741efcdb602c2f03d0a1611a586bd15be
Ketan Sonbawane <sonbawaneketan@gmail.com>
2026-08-28 22:32:29 +0530

    Time: 446 ms (60.59%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 select author_id as id
-from Views 
+from views 
 where author_id = viewer_id
-group by author_id 
-order by author_id
+
+group by author_id
+order by author_id asc
