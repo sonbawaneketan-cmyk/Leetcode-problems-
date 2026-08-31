73bb26d220db240f5992bb694a4c90b8a5ff9c9c
Ketan Sonbawane <sonbawaneketan@gmail.com>
2026-08-31 22:41:15 +0530

    Time: 2689 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,10 +1,10 @@
 SELECT
-    a.machine_id,
-    ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
-FROM Activity a
-JOIN Activity b
-    ON a.machine_id = b.machine_id
-   AND a.process_id = b.process_id
-WHERE a.activity_type = 'start'
-  AND b.activity_type = 'end'
-GROUP BY a.machine_id;
+    a1.machine_id,
+    ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+FROM Activity AS a1
+JOIN Activity AS a2
+    ON a1.machine_id = a2.machine_id
+    AND a1.process_id = a2.process_id
+    AND a1.activity_type = 'start'
+    AND a2.activity_type = 'end'
+GROUP BY a1.machine_id;
