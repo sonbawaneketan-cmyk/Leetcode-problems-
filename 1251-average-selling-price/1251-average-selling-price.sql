2985ddf35e939f509cc75976a6b69b897747aa20
Ketan Sonbawane <sonbawaneketan@gmail.com>
2026-09-03 22:55:47 +0530

    Time: 853 ms (66.63%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,11 +1,11 @@
-SELECT
+SELECT 
     p.product_id,
     ROUND(
-        COALESCE(SUM(p.price * u.units) / NULLIF(SUM(u.units), 0), 0),
+        COALESCE(SUM(p.price * u.units) / SUM(u.units), 0),
         2
     ) AS average_price
 FROM Prices AS p
 LEFT JOIN UnitsSold AS u
     ON p.product_id = u.product_id
-   AND u.purchase_date BETWEEN p.start_date AND p.end_date
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
