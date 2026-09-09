78329dc474b90654424068989b6133cf2158fbff
Ketan Sonbawane <sonbawaneketan@gmail.com>
2026-09-09 17:34:53 +0530

    Time: 557 ms (82.93%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,6 +1,8 @@
-select
-round(sum(player_login)/ count(distinct player_id),2) as fraction
+select 
+round(sum(plogin) / count(distinct player_id),2)as fraction
 from
-(select player_id ,
-datediff(event_date, min(event_date) over(partition by player_id ))=1 as player_login
+(select player_id,
+datediff(event_date,min(event_date )over (partition by player_id))=1 as plogin
 from activity) as new_table
+
+
