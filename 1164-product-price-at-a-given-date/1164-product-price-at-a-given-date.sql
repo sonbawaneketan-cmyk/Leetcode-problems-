930af5fc3c1df1f62ee8f42c2e3752ec05fdbe75
Ketan Sonbawane <sonbawaneketan@gmail.com>
2026-09-15 22:31:09 +0530

    Time: 1487 ms (9.44%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,20 +1,15 @@
-WITH latest_price AS (
-    SELECT
-        product_id,
-        new_price,
-        ROW_NUMBER() OVER (
-            PARTITION BY product_id
-            ORDER BY change_date DESC
-        ) AS rn
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-)
-
-SELECT
-    p.product_id,
-    COALESCE(l.new_price, 10) AS price
-FROM
-    (SELECT DISTINCT product_id FROM Products) p
-LEFT JOIN latest_price l
-ON p.product_id = l.product_id
-AND l.rn = 1;
+SELECT 
+    product_id,
+    COALESCE(
+        (
+            SELECT new_price
+            FROM Products p2
+            WHERE p2.product_id = p1.product_id
+              AND p2.change_date <= '2019-08-16'
+            ORDER BY p2.change_date DESC
+            LIMIT 1
+        ),
+        10
+    ) AS price
+FROM Products p1
+GROUP BY product_id;
