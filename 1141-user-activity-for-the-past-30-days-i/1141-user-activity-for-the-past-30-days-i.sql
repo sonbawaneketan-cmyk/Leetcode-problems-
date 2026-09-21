afc57aecd2cadf0dd185158c43631ccaec813b42
Ketan Sonbawane <sonbawaneketan@gmail.com>
2026-09-21 19:20:41 +0530

    Time: 624 ms (23.09%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,6 @@
-select activity_date as day,
-COUNT(DISTINCT user_id) AS active_users
-from activity 
-where activity_date between '2019-06-28'and '2019-07-27'
-group by activity_date
+SELECT 
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date;
